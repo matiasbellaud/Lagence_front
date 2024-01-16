82b963624d624444dd40177b8e58adfcd9b3dc96
levan <evan.lebourhis@ynov.com>
2024-01-16 14:22:25 +0100

    configuration page

--- my-app/src/components/Configuration.js
+++ my-app/src/components/Configuration.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./Configuration.css";
 
 const Configuration = () => {
@@ -16,9 +17,26 @@ const Configuration = () => {
                     <input type="text" name="city2" placeholder="ville 2" />
                     <input type="text" name="équipe" placeholder="équipe" />
                     <input type="text" name="vehicule" placeholder="vehicule" />
-                    <button className="btn-submit" type="submit" /*onClick={formData}*/>
+                    <Link to="/explorer">
+                    <button className="btn-submit" type="submit">
                             Envoyer
                     </button>
+                    </Link>
+                </form>
+            </div>
+            <div className="pouvoir">
+                <h2 className="title-pouvoir">Pouvoir(s)</h2>
+            <form className="form-pouvoir">
+                    <input type="text" name="nom" placeholder="" />
+                    <input type="text" name="description" placeholder="" />
+                    <input type="text" name="image" placeholder="" />
+            </form>
+            </div>
+            <div className="gadget">
+                <h2 className="title-gadget">Gadget(s)</h2>
+                <form className="form-gadget">
+                    <input type="text" name="nom" placeholder="" />
+                    <input type="text" name="description" placeholder="" />
                 </form>
             </div>
         </main>
